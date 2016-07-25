915c0064bed66e918224188cf01fc7ee6b38dcbc
Vanessa McHale <tmchale@wisc.edu>
2016-07-25 15:41:33 -0500

    fixed python wrapper

--- haskemd.py
+++ haskemd.py
@@ -6,18 +6,8 @@ from os.path import isfile
 from PIL import Image
 from fractions import Fraction
 
-sinksrand = randint(0,8192,8192,np.uint32)
-sourcesrand = randint(0,8192,8192,np.uint32)
-
-##note: np.float(5.9975).is_integer() does in fact work! use for lcm!:
-
-def fixlists (list1, list2):
-    np.dtype(Fraction).limit_denominator().denominator
-    
-    arr = numpy.concatenate(list1, list2)
-    multiplier = nump.amax(arr)
-    return multiplier
-##bascially, pick the biggest "second" and this is the multiplier
+sinksrand = randint(0,1024,1024,np.uint32)
+sourcesrand = randint(0,1024,1024,np.uint32)
 
 def tobmp (lists, filename):
     arr = np.uint32(np.int32(lists))
@@ -36,12 +26,12 @@ def emd(list1, list2):
     return haskemd("list1.bmp","list2.bmp","data/matrix-"+(str(list1.size)))
 
 def haskemd(list1, list2, matrix):
-    subprocess.call(["sudo","bin/edmonds-karp",str(list1),str(list2),matrix])
+    subprocess.call(["stack", "exec", "EMD-exe",str(list1),str(list2),matrix])
     cleanup()
 
 def makemat(l):
     dim = l.size
-    subprocess.call (["bin/matrixbmp",str(dim),"data/matrix-"+str(dim)])
+    subprocess.call (["stack","exec","Mat-exe",str(dim),"data/matrix-"+str(dim)])
 
 def cleanup():
     ##subprocess.call (["rm","matrix-*"])
