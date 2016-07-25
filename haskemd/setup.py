447d0968ddeffb8733b88dd74e6aec9bcba38223
Vanessa McHale <tmchale@wisc.edu>
2016-07-25 16:55:35 -0500

    package python code

---
+++ haskemd/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='haskemd',
+      version='0.1',
+      description='Python wrapper for CUDA-based EMD',
+      url='http://github.com/vmchale/EMD#readme',
+      author='Vanessa McHale',
+      author_email='tmchale@wisc.edu',
+      license='BSD3',
+      packages=['haskemd'],
+      zip_safe=False)
